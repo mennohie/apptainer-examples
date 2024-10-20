e5f9441fbbe13b5e338dc0ab6a4b37b1e668ba2d
mennohielkema <M.Hielkema-1@student.tudelft.nl>
2024-10-20 23:00:36 +0200

    Fixed 2 and 3

---
+++ 3-tmp-files/app/main.py
@@ -0,0 +1,44 @@
+import json
+import argparse
+import dotenv
+import os
+import pickle
+import numpy as np
+
+dotenv.load_dotenv()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Rolls",
+        description="Returns an embedding and an index vector for \
+                    any input dataframe containing protein sequences"
+    )
+    parser.add_argument('-N', '--number', required=True, type=int)
+    parser.add_argument('-u', '--url', required=True)
+    args = parser.parse_args()
+    N = args.number
+    url = args.url
+    if "seed" in os.environ:
+        if not os.environ["seed"]:
+            seed = None
+        else:
+            seed = int(os.environ["seed"])
+    else:
+        seed = None
+    np.random.seed(seed)
+    values = np.random.randint(1, 6, N)
+    names = os.environ["names"].split(", ")
+    with open("/opt/dicerolls.pkl", "wb") as f:
+        pickle.dump(values, f)
+    rolls = {}
+    for i, v in enumerate(values):
+        name = names[i % len(names)]
+        if name in rolls:
+            rolls[name] += int(v)
+        else:
+            rolls[name] = int(v)
+            
+    with open(url, "w", encoding='utf-8') as f:
+        json.dump(rolls, f)
+        
+        
